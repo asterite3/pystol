3631994e9ea2e988ed923628deb9dc308f921b41
asterite <asterite@seclab.cs.msu.su>
2020-04-13 01:58:01 +0300

    Update entry point

--- pystol.py
+++ pystol.py
@@ -1,16 +1,41 @@
-import sys
+import shlex
 
 from attach import attach_to_python_process
+import plugins
+import args
 
-control_transport, stdio_transport = attach_to_python_process(int(sys.argv[1]))
+def main():
+    target_pid, interactive, arguments, parser = args.parse_args()
 
-control_transport.send('''
-import code
-respond('ok')
-code.interact()
-''')
+    control_transport, stdio_transport = attach_to_python_process(target_pid)
 
-print(control_transport.recv())
-stdio_transport.in_transport.pipe_from(sys.stdin, 1)
+    if not interactive:
+        plugins.plugins[arguments.plugin].run(
+            control_transport,
+            stdio_transport,
+            arguments
+        )
+        return
+    try:
+        raw_input
+    except NameError:
+        raw_input = input
+    while True:
+        try:
+            command = raw_input('pystol> ')
+        except EOFError:
+            break
+        if command == '':
+            continue
+        try:
+            command_args = parser.parse_args(shlex.split(command))
+        except args.ArgumentParseError:
+            continue
+        plugins.plugins[command_args.plugin].run(
+            control_transport,
+            stdio_transport,
+            command_args
+        )
 
-stdio_transport.in_transport.thread.join()
+if __name__ == '__main__':
+    main()
