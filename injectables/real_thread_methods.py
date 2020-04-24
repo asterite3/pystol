7f97113180c1b604dec66a7c9d74605dbffa5d9c
asterite <asterite@seclab.cs.msu.su>
2020-04-24 23:43:58 +0300

    Import allocate_lock robustly to python3/ gevent

--- injectables/real_thread_methods.py
+++ injectables/real_thread_methods.py
@@ -6,6 +6,7 @@ except ImportError:
 
 start_new_thread = thread.start_new_thread
 get_ident = thread.get_ident
+allocate_lock = thread.allocate_lock
 
 if "gevent.monkey" in sys.modules:
     for thread_module in ["thread", "_thread"]:
@@ -13,3 +14,5 @@ if "gevent.monkey" in sys.modules:
             start_new_thread = sys.modules["gevent.monkey"].get_original(thread_module, "start_new_thread")
         if sys.modules["gevent.monkey"].is_object_patched(thread_module, "get_ident"):
             get_ident = sys.modules["gevent.monkey"].get_original(thread_module, "get_ident")
+        if sys.modules["gevent.monkey"].is_object_patched(thread_module, "allocate_lock"):
+            allocate_lock = sys.modules["gevent.monkey"].get_original(thread_module, "allocate_lock")
