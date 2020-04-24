28dd7e19e2798ad00a8ca4c1965b8199abce3c06
asterite <asterite@seclab.cs.msu.su>
2020-04-24 23:15:43 +0300

    Add closure getter

---
+++ modules/debugger/get_closure.py
@@ -0,0 +1,18 @@
+import gc, inspect
+
+def get_closure(frame):
+    code = frame.f_code
+
+    for ob in gc.get_referrers(code):
+        if inspect.isfunction(ob):
+            func = ob
+            break
+    else:
+        return {}
+
+    if hasattr(func, '__closure__'):
+        closure = func.__closure__
+    else:
+        closure = func.func_closure
+
+    return dict(zip(code.co_freevars, (c.cell_contents for c in closure)))
